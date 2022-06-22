819ad7891b1721774c7cbcb3c0e666a5d308fb5a
loic beernaert <loic.beernaert@gmail.com>
2022-06-22 12:10:04 +0200

    Ajout de la gestion du panier dans la page produit

--- front/js/index.js
+++ front/js/index.js
@@ -8,36 +8,39 @@ async function getProducts(){
 let productList = getProducts()
 .then((product) =>{
 
-// Boucle de creation
-for (let i=0; i < product.length; i++) {
+    // Boucle de creation
+    for (let i=0; i < product.length; i++) {
 
-    // Creation element lien
-    let productLink = document.createElement('a');
-    items.appendChild(productLink);
-    productLink.href = `product.html?id=${product[i]._id}`;
+        // Creation element lien
+        let productLink = document.createElement('a');
+        items.appendChild(productLink);
+        productLink.href = `product.html?id=${product[i]._id}`;
 
-    // Creation element Article
-    let productArticle = document.createElement('article');
-    productLink.appendChild(productArticle);
+        // Creation element Article
+        let productArticle = document.createElement('article');
+        productLink.appendChild(productArticle);
 
-    // creation element image
-    let productImg = document.createElement('img');
-    productArticle.appendChild(productImg);
-    productImg.classList.add('productImg');
-    productImg.src = product[i].imageUrl;
-    productImg.alt = product[i].altTxt;
+        // creation element image
+        let productImg = document.createElement('img');
+        productArticle.appendChild(productImg);
+        productImg.classList.add('productImg');
+        productImg.src = product[i].imageUrl;
+        productImg.alt = product[i].altTxt;
 
-    // Creation element nom
-    let productName = document.createElement('h3');
-    productArticle.appendChild(productName);
-    productName.classList.add('productName');
-    productName.innerHTML = product[i].name;
+        // Creation element nom
+        let productName = document.createElement('h3');
+        productArticle.appendChild(productName);
+        productName.classList.add('productName');
+        productName.innerHTML = product[i].name;
 
-    // Creation element description
-    let productDescription = document.createElement('p');
-    productArticle.appendChild(productDescription);
-    productDescription.classList.add('productDescription');
-    productDescription.innerHTML = product[i].description;
+        // Creation element description
+        let productDescription = document.createElement('p');
+        productArticle.appendChild(productDescription);
+        productDescription.classList.add('productDescription');
+        productDescription.innerHTML = product[i].description;
     }
 })
-.catch((err) => console.log('Erreur ' + err));
+// Console log en cas d'erreur de fetch
+.catch ((error) =>{
+    console.log (error);
+});
--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,5 @@
 // Recuperation de l'id du produit contenu dans l'URL sous la forme de variable "productID"
 const productID = new URL(window.location.href).searchParams.get("id");
-console.log(productID)
 
 // Constructeur fonction pour retourner les data du produit lie a l'ID
 async function getProduct(identifiant_produit){
@@ -12,9 +11,6 @@ async function getProduct(identifiant_produit){
 // et l'injecte dans le DOM
 getProduct(productID)
 .then(product => {
-
-    //check up des data dans la console
-    console.log(product);
 
     // Creation element img depuis sa class item__img
     let productImg = document.createElement("img")
@@ -35,6 +31,45 @@ getProduct(productID)
         productColors.innerHTML = colors;
     }
 })
+// Console log en cas d'erreur de fetch
+.catch ((error) => {
+    console.log(error);
+})
 
 
 
+// Gestion du panier
+
+//Fonction qui permet de sauvgarder le parametre basket dans le localStorage sous forme de string
+function saveBasket(basket) {
+    localStorage.setItem("basket", JSON.stringify(basket));
+} 
+
+//Fonction qui permet de retourner le panier sous forme JSON si existant, dans le cas contraire renvoie un tableau vide 
+function getBasket() {
+    let basket = localStorage.getItem("basket");
+    if (basket == null) {
+        return [];
+    } else {
+        return JSON.parse(basket);
+    }
+}
+
+//Evenement sur le clic du bouton Ajouter au panier
+addToCart.addEventListener("click", ()=>{
+    // Definition de l'array du produit
+    let product = {id:`${productID}`, color:`${colors.value}`, quantity:Number(`${quantity.value}`)};
+    // Recuperation du localstorage
+    let basket = getBasket();
+    // Verifier si le produit/couleur existe deja
+    let foundProduct = basket.find(p => (p.id == product.id) && (p.color == product.color));
+    //Si l eproduit/couleur ets deja present, ajoute la quantite
+    if(foundProduct != undefined){
+    foundProduct.quantity = Number(foundProduct.quantity) + Number(`${quantity.value}`);
+    // si inexistant, l'ajoute au local storage
+    } else {
+        basket.push(product);
+    }
+    // renvoi dans le localstorage
+    saveBasket(basket);
+})
